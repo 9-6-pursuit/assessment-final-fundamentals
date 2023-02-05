60386e59f61738d33153579f584e35db7397f720
Lakisha J <lakishajohnson@pursuit.org>
2023-02-05 09:32:51 -0500

    Got movie by id

--- index.js
+++ index.js
@@ -125,7 +125,15 @@ return ratedMovie
       // Toy Story 4
     };
  */
-function findById() {}
+function findById(movies, id) {
+  for (let i = 0; i < movies.length; i++) {
+    
+    if (movies[i].imdbID === id) {
+      return movies[i];
+    }
+  }
+  return null
+};
 
 /**
  * filterByGenre()
